aee7e014b14e08c1e8a62cb7eea39b00ebf3a359
krister <krister@sent.at>
2019-10-30 17:38:00 +0200

    New Expo project with Sass

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import styles from "./App.scss";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.test}>Open up App.tsx to start working on your app!</Text>
+    </View>
+  );
+}
+
+
